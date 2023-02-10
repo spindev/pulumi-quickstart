f215c09aeaf6f22a69d3adca9e4efb34abbe5f5d
Till Spindler <tillspi@web.de>
2023-02-10 20:29:19 +0000

    Test for local action executing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Pulumi;
+using Pulumi.AzureNative.Compute;
 using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
 using Pulumi.AzureNative.Storage.Inputs;
@@ -9,32 +10,37 @@ return await Pulumi.Deployment.RunAsync(() =>
     // Create an Azure Resource Group
     var resourceGroup = new ResourceGroup("resourceGroup");
 
-    // Create an Azure resource (Storage Account)
-    var storageAccount = new StorageAccount("sa", new StorageAccountArgs
-    {
+    var usi = new Pulumi.AzureNative.ManagedIdentity.UserAssignedIdentity("usi", new Pulumi.AzureNative.ManagedIdentity.UserAssignedIdentityArgs{
         ResourceGroupName = resourceGroup.Name,
-        Sku = new SkuArgs
-        {
-            Name = SkuName.Standard_LRS
-        },
-        Kind = Kind.StorageV2
+        ResourceName = "MyCustumUserIdentity"
     });
 
-    var storageAccountKeys = ListStorageAccountKeys.Invoke(new ListStorageAccountKeysInvokeArgs
-    {
-        ResourceGroupName = resourceGroup.Name,
-        AccountName = storageAccount.Name
-    });
+    // // Create an Azure resource (Storage Account)
+    // var storageAccount = new StorageAccount("sa", new StorageAccountArgs
+    // {
+    //     ResourceGroupName = resourceGroup.Name,
+    //     Sku = new SkuArgs
+    //     {
+    //         Name = SkuName.Standard_LRS
+    //     },
+    //     Kind = Kind.StorageV2
+    // });
 
-    var primaryStorageKey = storageAccountKeys.Apply(accountKeys =>
-    {
-        var firstKey = accountKeys.Keys[0].Value;
-        return Output.CreateSecret(firstKey);
-    });
+    // var storageAccountKeys = ListStorageAccountKeys.Invoke(new ListStorageAccountKeysInvokeArgs
+    // {
+    //     ResourceGroupName = resourceGroup.Name,
+    //     AccountName = storageAccount.Name
+    // });
 
-    // Export the primary key of the Storage Account
-    return new Dictionary<string, object?>
-    {
-        ["primaryStorageKey"] = primaryStorageKey
-    };
+    // var primaryStorageKey = storageAccountKeys.Apply(accountKeys =>
+    // {
+    //     var firstKey = accountKeys.Keys[0].Value;
+    //     return Output.CreateSecret(firstKey);
+    // });
+
+    // // Export the primary key of the Storage Account
+    // return new Dictionary<string, object?>
+    // {
+    //     ["primaryStorageKey"] = primaryStorageKey
+    // };
 });
